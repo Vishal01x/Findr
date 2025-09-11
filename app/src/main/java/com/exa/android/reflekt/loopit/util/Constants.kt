887da944298bd7f407693ca6cda041e66d9759f2
Vishal Dangi <148259513+Vishal01x@users.noreply.github.com>
2025-09-11 18:25:39 +0530

    Update Constants.kt

--- app/src/main/java/com/exa/android/reflekt/loopit/util/Constants.kt
+++ app/src/main/java/com/exa/android/reflekt/loopit/util/Constants.kt
@@ -2,23 +2,3 @@ package com.exa.android.reflekt.loopit.util
 
 import java.util.UUID
 
-object Constants {
-    const val MEET_API_KEY = "wt9xvrvup9nq"
-    const val callType = "default"
-     val callId = UUID.randomUUID()
-
-
-
-    const val CLOUDINARY_BASE_URL = "https://api.cloudinary.com/"
-    const val CLOUDINARY_UPLOAD_PRESET = "media_uploads"
-    const val CLOUDINARY_NAME = "dgqxusedq"
-
-    const val BRANDFTECH_BASE_URL = "https://api.brandfetch.io/"
-
-    const val APP_NAME = "Let's Talk"
-
-    const val CAMERA_REQUEST_CODE = 1010
-
-    const val FINDR_GROUP_KEY = "17895625959565findr.com"
-
-}
